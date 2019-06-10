2d7f6c31016f5f24a7515ffd622036310c7095f6
Matt <mattsully01@gmail.com>
2019-06-10 17:05:33 -0600

    restet all of my JS becuase original idea didnt work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,46 @@
-var gameQuestions =[
-    {
-        question:"which came first?",
-        answers: {
-            a: "xbox",
-            b: "Nes",
-            c: "Nitendo 64",
-            d: "GameBoy Color"
-        },
-        correctAnswer:"b"
-    },
-
-    {
-        question:"which of these dose not have an X button",
-        answers: {
-            a: "Playstation controller",
-            b: "GameCube controller",
-            c: "Nintendo 64 controller",
-            d: "Nintendo Switch pro controller"
-        },
-        correctAnswer:"c"
-    }
-];
-console.log(gameQuestions[1].question)
-
-function gameStart(){
-    displayQuestion()
-    displayBubbles()
+const quiz = {
+    question : "this is a question",
+    answer:['a', 'b' , 'c' , 'd'],
+    corectAnswer: 'a' 
 }
 
-gameStart()
+console.log(quiz.question)
 
-//write a function that displays the question on the dom
+// function that displays questoion 
 function displayQuestion(){
- var question1 = gameQuestions[0].question;
-  $("#question").text(question1);
-   
+    var display = $('<div>' + quiz.question + '</div>')
+    $('.game-container').append(display)
 }
 
-//write a function that displays 4 input bubbles
-function displayBubbles(){
+// function that displays choices as buttons
+function displayAnswers(){
+    var choices = quiz.answer
+    //cycle through the answers array
+    for( i = 0; i < choices.length ; i++){
+        var answerChoices = choices[i]
+        var display = $('<button>' + answerChoices + '</button>')
+        $('.game-container').append(display)
+        // console.log(answerChoices)
+    }
+    
+}
 
-    for (i = 0; i < 4; i++) {
-        console.log(output[1])
-        var button = $('<button class="btn btn-primary bt-lg"> </button>')
-        button.appendTo(".quizContainer")
-
-        
+function setBtnValues(){
+    var values = quiz.answer
+    for(i = 0; i < values.length; i++){
+        var value = values[i];
+        $('button').val(value)
+        console.log( $('button').val())
     }
 }
 
-function displaAnswers(){
-    var answer1 = [];
-    answer1.push(gameQuestions[0].answers)
-    for(i = 0; i < answer1.length; i++){
-        
-    }
+function onClick(){
+    $('button').on('click', function(){
+        console.log($('button').val())
+    })
 }
 
-
-//write a function that checks if the user input is correct or inccorect 
-    // then changes the question.
-
-
+displayQuestion()
+displayAnswers()
+setBtnValues()
+onClick()
